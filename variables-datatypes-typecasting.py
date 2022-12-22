9837ffbc2a3c1671c5e9a2720aac330fd8bbc447
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-22 15:18:46 +0600

    Learned about variables, datatypes and typecasting

---
+++ variables-datatypes-typecasting.py
@@ -0,0 +1,23 @@
+# name = "Mahidur Rahman"
+# print(name)
+# print(name)
+# print(name)
+# print(name)
+# print("My name is:", name)
+# print(type(name))  # type function use to define variable type
+# num_int = 10
+# num_float = 10.58
+# print(num_int)
+# print(num_float)
+# print("sum:", num_int+num_float)
+# var1 = "32"
+# var2 = "8"
+# print(var1+var2)
+# print(int(var1)+int(var2))  # typecast
+"""Different type of typecasting"""
+# str()
+# int()
+# float()
+
+# print(10*"Hello Python!!\n")
+# print(10*str(int(var1)+int(var2)))
