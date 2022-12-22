28d60e89b0fe2022034ae123c9a1c6295528342a
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-22 14:52:51 +0600

    Learned about the comments, escape-sequence and print function

---
+++ comments-scapeSequence-print.py
@@ -0,0 +1,22 @@
+# This is a single line comment
+
+# Printing my name
+# print('Mahidur Rahman')
+
+"""
+This is 
+use for 
+multiline comment
+"""
+
+# To ommit the default new line
+print("My name is: ", end="")
+print("Mahidur Rahman")
+
+# Escape sequence
+# print('Mahidur is \n a good \t boy.')
+
+# print("Hello", "Mahidur Rahamn")
+
+print("Hello", end=", ")
+print("Mahidur Rahman")
