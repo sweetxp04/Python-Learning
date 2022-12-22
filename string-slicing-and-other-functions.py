0bdf638553d3394171716aa290b6f9359ca66201
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-22 16:05:01 +0600

    Learned about string slicing and other string associated functions

---
+++ string-slicing-and-other-functions.py
@@ -0,0 +1,17 @@
+myStr = "Mahidur is a good boy"
+# print(myStr[0:8])
+# print(myStr[:8])
+# print(len(myStr))
+# print(myStr[5])
+# print(myStr[::2])
+# print(myStr[::-1]) # Reversing the string
+# print(myStr.endswith("boy"))
+# print(myStr.isalnum())
+# print(myStr.isalpha())
+# print(myStr.count("o"))
+# print(myStr.upper())
+# print(myStr.lower())
+# print(myStr.capitalize())
+# print(myStr.swapcase())
+# newStr = myStr.replace("is", "are")
+# print(newStr)
