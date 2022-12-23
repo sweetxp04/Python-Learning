d2fc42a5521ac870322743f177bbc1cb52ecccc4
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-23 15:44:53 +0600

    Learned about python tuple

---
+++ python-tuple.py
@@ -0,0 +1,15 @@
+# my_cars is a list and can change the entity
+# my_cars = ["Ford", "Mercedes", "Audi"]
+# my_cars[0] = "Ford Titanium"
+# print(my_cars)
+# my_cars is a tuple and can't change the entity
+my_cars = ("Ford", "Mercedes", "Audi")
+# my_cars[0] = "Ford Titanium"  # get the error
+# print(my_cars)
+# print(type(my_cars))
+# print(my_cars[1])
+# List = list(my_cars)
+# print(List)
+# List[0] = "Mahidur"
+# my_cars = tuple(List)
+# print(my_cars)
