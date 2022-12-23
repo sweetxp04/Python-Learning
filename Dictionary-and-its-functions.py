3a3f0c67f59fe14541e92eb2fe687d6200f43dd3
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-23 16:23:51 +0600

    Learned about python dictionary and its associated functions

---
+++ Dictionary-and-its-functions.py
@@ -0,0 +1,28 @@
+# Dictionary is nothing but key value pairs
+
+# my_dict = {}  #blank dictionary
+# print(type(my_dict))
+
+my_dict = {"Mahidur": "Burger", "Monmon": "Mutton", "Rahman": "Fish", "Sarker": {
+    "B": "Roti",
+    "L": "Rice",
+    "D": "Chicken"
+}, "Tanbir": "Junk Foor", "Shanto": "Dry Food"}
+
+# print(my_dict)
+# print(my_dict["Mahidur"])
+# print(my_dict["Monmon"])
+# print(my_dict["Sarker"]["B"])
+# adding elements to dictionary
+# print(my_dict)
+# del my_dict["Shanto"]
+print(my_dict)
+
+# d2 = my_dict.copy()
+# del d2["Mahidur"]
+# print(d2)
+# print(my_dict)
+# my_dict.update({"Moja": "Peyaju"})
+# print(my_dict)
+# print(my_dict.keys())
+# print(type(my_dict.keys()))
