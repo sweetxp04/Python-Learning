7f8359d51bde2910bd62fc93e384386cce7f0ebe
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-23 15:57:24 +0600

    Learned about python tuple and create gitignore file

--- python-tuple.py
+++ python-tuple.py
@@ -3,7 +3,7 @@
 # my_cars[0] = "Ford Titanium"
 # print(my_cars)
 # my_cars is a tuple and can't change the entity
-my_cars = ("Ford", "Mercedes", "Audi")
+my_cars = ("Ford", "Mercedes", "Audi", "Lamborghini")
 # my_cars[0] = "Ford Titanium"  # get the error
 # print(my_cars)
 # print(type(my_cars))
