98040c11e3cc5d805e7391dcb36e37558934a2e0
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-27 22:47:32 +0600

    Learned about functions

---
+++ functions.py
@@ -0,0 +1,68 @@
+""""def generate_full_name():
+    first_name = "Mahidur"
+    last_name = "Rahman"
+    space = " "
+    full_name = first_name + space + last_name
+    print(full_name)
+
+
+generate_full_name()"""
+
+""""def add_two_numbers():
+    num_one = 5
+    num_two = 3
+    total = num_one + num_two
+    print(total)
+
+
+add_two_numbers()"""
+
+""""def generate_full_name():
+    first_name = "Mahidur"
+    last_name = "Rahman"
+    space = " "
+    full_name = first_name + space + last_name
+    return full_name
+
+
+print(generate_full_name())"""
+
+""""def greetings(name):
+    message = f"{name}, welcome to python world!!"
+    return message
+
+
+print(greetings("Mahidur"))"""
+
+""""def sum_of_numbers(n):
+    total = 0
+    for i in range(1, n + 1):
+        total += i
+    return total
+
+
+print(sum_of_numbers(100))"""
+
+""""def generate_full_name(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    return full_name
+
+
+print(generate_full_name("Mahidur", "Rahman"))"""
+
+""""def sum_two_numbers(num_one, num_two):
+    total = num_one + num_two
+    return total
+
+
+print(sum_two_numbers(10, 20))"""
+
+
+"""def greetings(name="Mahidur"):
+    message = f"{name}, welcome to python world!!"
+    return message
+
+
+print(greetings())
+print(greetings("Rahman"))"""
+
