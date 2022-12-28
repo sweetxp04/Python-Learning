cbf3fd7e4af1b8212afce7ead89858f916601742
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-28 12:59:19 +0600

    Added new code

--- list-comprehension.py
+++ list-comprehension.py
@@ -1,3 +1,21 @@
 name = "Mahidur"
-lst_name = [i for i in name]
-print(lst_name)
+# lst_name = [i for i in name]
+# print(lst_name)
+
+# numbers = [i for i in range(0, 11)]
+# print(numbers)
+# It is possible to do mathematical operations during iteration
+# squares = [i * i for i in range(1, 11) if i != 5]
+# print(squares)
+#
+# lst_tuple = [(i, i * i) for i in range(11)]
+# print(lst_tuple)
+# even_numbers = [i for i in range(20) if i % 2 == 0]
+# print(even_numbers)
+# odd_numbers = [i for i in range(1, 20) if i % 2 != 0]
+# print(odd_numbers)
+# Flattening a three-dimensional array
+# list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# flattend_list = [item for row in list_of_lists for item in row]
+# print(flattend_list)
+
